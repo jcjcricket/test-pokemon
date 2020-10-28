c4cc87558075df35aa97511acccb6fe1b037c1b2
J CRICKET <sssssssnk7@gmail.com>
2020-10-28 14:58:49 +0200

    Updates

---
+++ precache-manifest.a52991489c9bdaa76f33f895d90ff32f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae9dcf02b66c0cafac292e017a3e714",
+    "url": "/test-pokemon/index.html"
+  },
+  {
+    "revision": "546ac7be9d3a8db6aea6",
+    "url": "/test-pokemon/static/css/main.7664bf82.chunk.css"
+  },
+  {
+    "revision": "5eb41b141f893d4b1ce7",
+    "url": "/test-pokemon/static/js/2.f00e6ef4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test-pokemon/static/js/2.f00e6ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "546ac7be9d3a8db6aea6",
+    "url": "/test-pokemon/static/js/main.f8dab9d8.chunk.js"
+  },
+  {
+    "revision": "776ff7a8bc87d47d7c6b",
+    "url": "/test-pokemon/static/js/runtime-main.b6cc5428.js"
+  },
+  {
+    "revision": "2acc1fed4ffc5c26f4dd89c0636779bb",
+    "url": "/test-pokemon/static/media/background.2acc1fed.jpg"
+  }
+]);
